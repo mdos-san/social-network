38c3e94119ae25acedc7302d5a0ea55ebac94fe3
mdos-san <mdossan@protonmail.com>
2023-06-23 19:28:29 +0200

    tech: Group jest.config.js files

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,6 +1,1 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
-};
+../config/jest.config.js
