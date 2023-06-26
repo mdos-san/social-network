ed6cd2c699431ba1722cb02ce50f843cbeb8b9dd
mdos-san <mdossan@protonmail.com>
2023-06-26 23:15:19 +0200

    feat(front): Redirect to profile page after authentification
    Close #28

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,71 +1,119 @@
-import { Formik, FormikErrors } from "formik";
 import axios from "axios";
+import { Formik, FormikErrors } from "formik";
+import { ReactNode, useEffect } from "react";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/authentification",
+    element: <AuthPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+]);
 
 interface LoginFormValues {
   login: string;
   password: string;
 }
 
-function App() {
+function ProfilePage(): ReactNode {
   return (
-    <Formik<LoginFormValues>
-      initialValues={{ login: "", password: "" }}
-      validate={(values) => {
-        const errors = {} as FormikErrors<LoginFormValues>;
-
-        if (!values.login) {
-          errors.login = "Required";
-        } 
-
-        if (!values.password) {
-          errors.password = "Required";
-        } 
-
-        return errors;
-      }}
-      onSubmit={async (values, { setSubmitting }) => {
-          const sessionResponse = await axios.post("http://127.0.0.1:3000/session", values, { withCredentials: true })
-          console.log(sessionResponse);
-          const userInfoResponse = await axios.get("http://127.0.0.1:3000/userinfo", { withCredentials: true })
-          console.log(userInfoResponse);
-          setSubmitting(false);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="login"
-            name="login"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.login}
-          />
-          {errors.login && touched.login && errors.login}
-
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
+    <div>
+      <h1>Profile</h1>
+    </div>
   );
+}
+
+function AuthPage(): ReactNode {
+  return (
+    <div>
+      <h1>Authentification</h1>
+      <Formik<LoginFormValues>
+        initialValues={{ login: "", password: "" }}
+        validate={(values) => {
+          const errors = {} as FormikErrors<LoginFormValues>;
+
+          if (!values.login) {
+            errors.login = "Required";
+          }
+
+          if (!values.password) {
+            errors.password = "Required";
+          }
+
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          await axios.post("http://127.0.0.1:3000/session", values, {
+            withCredentials: true,
+          });
+          await axios.get("http://127.0.0.1:3000/userinfo", {
+            withCredentials: true,
+          });
+          setSubmitting(false);
+          window.location.hash = "/profile";
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="login"
+              name="login"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.login}
+            />
+            {errors.login && touched.login && errors.login}
+
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const userInfoResponse = await axios.get(
+          "http://127.0.0.1:3000/userinfo",
+          { withCredentials: true }
+        );
+        console.log("INIT", userInfoResponse);
+        window.location.hash = "/profile";
+      } catch (e) {
+        window.location.hash = "/authentification";
+      }
+    })();
+  }, []);
+
+  // TODO: Loader
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
